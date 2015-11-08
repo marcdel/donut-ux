a213dc16e927a4514ce716c8a76f704d4b730a06
Marc Delagrammatikas <delagrammatikas@gmail.com>
2015-11-07 17:31:05 -0800

    Add basic angular app framework.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'home/index'
+
   root 'static_pages#home'
+  get 'static_pages/home'
+  get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
