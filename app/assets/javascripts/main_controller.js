7e457c79972787352451445cc06195ff8a128f17
Marc Delagrammatikas <delagrammatikas@gmail.com>
2015-11-18 20:22:07 -0800

    Layout changes.
    Replace the donuts with background colors for now. Adjust padding and
    heights and stuff.

--- app/assets/javascripts/main_controller.js
+++ app/assets/javascripts/main_controller.js
@@ -1,3 +1,4 @@
 app.controller('MainController', ['$scope', function($scope){
   $scope.title = "Donut UX: A Full Circle UX Agency";
+  $scope.lipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet eleifend eros. Sed fermentum et eros non cursus. Proin tempor lobortis nisl, vel consequat turpis sagittis at. Ut blandit molestie lobortis. Curabitur magna arcu, condimentum finibus velit non, commodo semper elit. Donec ut consectetur velit. Proin sagittis odio maximus, interdum orci sed, imperdiet urna.";
 }]);
