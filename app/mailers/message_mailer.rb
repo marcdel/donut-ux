05fbb8ef3d2798edd296af9511c0b5b2a746e435
Marc Delagrammatikas <delagrammatikas@gmail.com>
2015-12-10 11:42:43 -0800

    Add send_message method to mailer.

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,2 +1,8 @@
 class MessageMailer < ApplicationMailer
+  default :to => ['jaqtann@gmail.com','aliciajolivares@gmail.com']
+  def send_message message
+    @message = message
+
+    mail from: @message.email, subject: @message.subject, body: @message.content
+  end
 end
--- spec/mailers/message_mailer_spec.rb
+++ spec/mailers/message_mailer_spec.rb
@@ -1,5 +1,23 @@
 require "rails_helper"
 
 RSpec.describe MessageMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'sucessfully sends messages' do
+    valid_attributes = {
+      name: 'marc',
+      email: 'test@email.com',
+      subject: 'hello',
+      content: 'test test test'
+    }
+
+    message = Message.new valid_attributes
+
+    email = MessageMailer.send_message(message).deliver_now
+
+    expect(ActionMailer::Base.deliveries).not_to be_empty
+
+    expect(email.to).to eq(['jaqtann@gmail.com','aliciajolivares@gmail.com'])
+    expect(email.from).to eq(['test@email.com'])
+    expect(email.subject).to eq('hello')
+    expect(email.body.to_s).to eq('test test test')
+  end
 end
