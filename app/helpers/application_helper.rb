d8fcccd2013aa274b56e2fa74c2ffc79dc86158b
Marc Delagrammatikas <delagrammatikas@gmail.com>
2015-12-11 10:53:46 -0800

    Add flash message logic to main layout.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def bootstrap_class_for flash_type
+    case flash_type
+      when "notice"
+        "alert-info"
+      else
+        "alert-#{flash_type}"
+    end
+  end
 end
